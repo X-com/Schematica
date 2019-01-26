ce4974f8244c60e493a6b2555153cfc35b56bf59
Unknown <sahandhesar@gmail.com>
2019-01-26 12:13:47 +0100

    small refactor to RenderChunk

--- src/main/java/mixin/MixinRenderChunk.java
+++ src/main/java/mixin/MixinRenderChunk.java
@@ -1,6 +1,6 @@
 package mixin;
 
-import com.github.lunatrius.schematica.util.IRenderChunkAccessor;
+import mixininterfaces.IRenderChunkAccessor;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.chunk.RenderChunk;
 import net.minecraft.util.math.BlockPos;
@@ -11,10 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderChunk.class)
 public class MixinRenderChunk {
+    private final boolean isRenderChunk = this instanceof IRenderChunkAccessor;
 
     @Inject(method = "preRenderBlocks", at = @At("HEAD"), cancellable = true)
     private void cheatyPreRenderBlocks(BufferBuilder buffer, BlockPos pos, CallbackInfo ci) {
-        if (this instanceof IRenderChunkAccessor) {
+        if (isRenderChunk) {
             ((IRenderChunkAccessor) this).preRenderBlocks(buffer, pos);
             ci.cancel();
         }
