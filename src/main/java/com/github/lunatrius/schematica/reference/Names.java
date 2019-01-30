def5a484f8e1c6ceff51e7c636f6fb55fa61dc3f
Unknown <sahandhesar@gmail.com>
2019-01-30 10:49:56 +0100

    Edited text of names in config

--- src/main/java/com/github/lunatrius/schematica/reference/Names.java
+++ src/main/java/com/github/lunatrius/schematica/reference/Names.java
@@ -12,57 +12,58 @@ public final class Names {
             public static final String SERVER = "server";
         }
 
-        public static final String DUMP_BLOCK_LIST = "dumpBlockList";
+        public static final String DUMP_BLOCK_LIST = "Dump Block List";
         public static final String DUMP_BLOCK_LIST_DESC = "Dump all block states on startup.";
-        public static final String SHOW_DEBUG_INFO = "showDebugInfo";
+        public static final String SHOW_DEBUG_INFO = "Show Debug Info";
         public static final String SHOW_DEBUG_INFO_DESC = "Display extra information on the debug screen (F3).";
 
-        public static final String ALPHA_ENABLED = "alphaEnabled";
+        public static final String ALPHA_ENABLED = "Alpha Enabled";
         public static final String ALPHA_ENABLED_DESC = "Enable transparent textures.";
-        public static final String ALPHA = "alpha";
+        public static final String ALPHA = "Alpha";
         public static final String ALPHA_DESC = "Alpha value used when rendering the schematic (1.0 = opaque, 0.5 = half transparent, 0.0 = transparent).";
-        public static final String HIGHLIGHT = "highlight";
+        public static final String HIGHLIGHT = "Highlight Blocks";
         public static final String HIGHLIGHT_DESC = "Highlight invalid placed blocks and to be placed blocks.";
-        public static final String HIGHLIGHT_AIR = "highlightAir";
+        public static final String HIGHLIGHT_AIR = "Highlight Air";
         public static final String HIGHLIGHT_AIR_DESC = "Highlight blocks that should be air.";
-        public static final String BLOCK_DELTA = "blockDelta";
+        public static final String BLOCK_DELTA = "Block Delta";
         public static final String BLOCK_DELTA_DESC = "Delta value used for highlighting (if you experience z-fighting increase this).";
-        public static final String RENDER_DISTANCE = "renderDistance";
+        public static final String RENDER_DISTANCE = "Render Distance";
         public static final String RENDER_DISTANCE_DESC = "Schematic render distance.";
 
-        public static final String PLACE_DELAY = "placeDelay";
+        public static final String PLACE_DELAY = "Place Delay";
         public static final String PLACE_DELAY_DESC = "Delay between placement attempts (in ticks).";
-        public static final String TIMEOUT = "timeout";
+        public static final String TIMEOUT = "Timeout";
         public static final String TIMEOUT_DESC = "Timeout before re-trying failed blocks.";
-        public static final String PLACE_DISTANCE = "placeDistance";
+        public static final String PLACE_DISTANCE = "Place Distance";
         public static final String PLACE_DISTANCE_DESC = "Maximum placement distance.";
-        public static final String PLACE_INSTANTLY = "placeInstantly";
+        public static final String PLACE_INSTANTLY = "Place Instantly";
         public static final String PLACE_INSTANTLY_DESC = "Place all blocks that can be placed in one tick.";
-        public static final String DESTROY_BLOCKS = "destroyBlocks";
+        public static final String DESTROY_BLOCKS = "Destroy Blocks";
         public static final String DESTROY_BLOCKS_DESC = "The printer will destroy blocks (creative mode only).";
-        public static final String DESTROY_INSTANTLY = "destroyInstantly";
+        public static final String DESTROY_INSTANTLY = "Destroy Instantly";
         public static final String DESTROY_INSTANTLY_DESC = "Destroy all blocks that can be destroyed in one tick.";
-        public static final String PLACE_ADJACENT = "placeAdjacent";
+        public static final String PLACE_ADJACENT = "Place Adjacent";
         public static final String PLACE_ADJACENT_DESC = "Place blocks only if there is an adjacent block next to them.";
-        public static final String SWAP_SLOT = "swapSlot";
+        public static final String SWAP_SLOT = "Swap Slot";
         public static final String SWAP_SLOT_DESC = "Allow the printer to use this hotbar slot.";
+        public static final String HOTBAR_SLOTS = "Hotbar Slots";
 
-        public static final String SCHEMATIC_DIRECTORY = "schematicDirectory";
+        public static final String SCHEMATIC_DIRECTORY = "Schematic Directory";
         public static final String SCHEMATIC_DIRECTORY_DESC = "Schematic directory.";
-        public static final String EXTRA_AIR_BLOCKS = "extraAirBlocks";
+        public static final String EXTRA_AIR_BLOCKS = "Extra Air Blocks";
         public static final String EXTRA_AIR_BLOCKS_DESC = "Extra blocks to consider as air for the schematic renderer.";
-        public static final String SORT_TYPE = "sortType";
+        public static final String SORT_TYPE = "Sort Type";
         public static final String SORT_TYPE_DESC = "Default sort type for the material list.";
 
-        public static final String PRINTER_ENABLED = "printerEnabled";
-        public static final String PRINTER_ENABLED_DESC = "Allow players to use the printer.";
-        public static final String SAVE_ENABLED = "saveEnabled";
-        public static final String SAVE_ENABLED_DESC = "Allow players to save schematics.";
-        public static final String LOAD_ENABLED = "loadEnabled";
-        public static final String LOAD_ENABLED_DESC = "Allow players to load schematics.";
-
-        public static final String PLAYER_QUOTA_KILOBYTES = "playerQuotaKilobytes";
-        public static final String PLAYER_QUOTA_KILOBYTES_DESC = "Amount of storage provided per-player for schematics on the server.";
+//        public static final String PRINTER_ENABLED = "printerEnabled";
+//        public static final String PRINTER_ENABLED_DESC = "Allow players to use the printer.";
+//        public static final String SAVE_ENABLED = "saveEnabled";
+//        public static final String SAVE_ENABLED_DESC = "Allow players to save schematics.";
+//        public static final String LOAD_ENABLED = "loadEnabled";
+//        public static final String LOAD_ENABLED_DESC = "Allow players to load schematics.";
+//
+//        public static final String PLAYER_QUOTA_KILOBYTES = "playerQuotaKilobytes";
+//        public static final String PLAYER_QUOTA_KILOBYTES_DESC = "Amount of storage provided per-player for schematics on the server.";
 
         public static final String LANG_PREFIX = Reference.MODID + ".config";
     }
